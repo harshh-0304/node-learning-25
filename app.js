0a5f5833bcb18ee39da7c0107ab9bedff57e9c79
harshh <harshjadav332004@gmail.com>
2025-02-25 10:04:27 +0530

    25 FEB

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
-const express = require("express"); // Import express
-const app = express(); // Create an express app
+const express = require("express")
+const mongoose = require("mongoose")
 
-// Middleware to parse JSON requests (optional but recommended)
-app.use(express.json());
+const app = express()
+const roleRoutes = require ("./src/routes/RoleRoutes")
+app.use(roleRoutes)
 
-// Test API
-// http://localhost:3000/test
-app.get("/test", (req, res) => {
-    console.log("Test API called...");
-    res.send("Hello, test API called...");
-});
 
-// Users API
-// http://localhost:3000/users
-app.get("/users", (req, res) => {
-    res.json({
-        message: "User API called...",
-        data: ["ram", "shyam", "seeta"]
-    });
-});
 
-// Employees API
-// http://localhost:3000/employees
-app.get("/employees", (req, res) => {
-    res.json({
-        message: "Employee API called...",
-        data: [
-            { id: 1, name: "John Doe", position: "Software Engineer" },
-            { id: 2, name: "Jane Smith", position: "Project Manager" },
-            { id: 3, name: "Alice Johnson", position: "UX Designer" }
-        ]
-    });
-});
-
-// Server creation
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log("Server started on port number", PORT);
-});
+mongoose.connect("mongodb://localhost:27017/25_node_internship").then(()=>{
+    console.log("database connected....")
+})
+const PORT = 3000
+app.listen(PORT,()=>{
+    console.log("server started on port number ",PORT)
+})
